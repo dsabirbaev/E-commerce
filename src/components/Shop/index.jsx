208dde23e001a89ef3405cbd2caa929188fb1705
Davranbek <davranbek_1995@mail.ru>
2023-08-20 20:07:36 +0500

    added section shop

--- src/components/Shop/index.jsx
+++ src/components/Shop/index.jsx
@@ -4,12 +4,107 @@
 
 import "./style.scss";
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/bundle';
+
 const index = () => {
     return (
-        <section>
-           <div className="container">
-                <h1>Shop</h1>
-           </div> 
+        <section className="pt-[100px]">
+            <div className="container">
+                <div>
+                    <h2 className="text-[36px] font-semibold text-[#111827]">Shop by department</h2>
+
+                    <Swiper
+                        spaceBetween={20}
+                        slidesPerView={4}
+                        modules={[Navigation]}
+                        navigation={{
+                            nextEl: ".button-next-slide",
+                            prevEl: ".button-prev-slide",
+                        }}
+                        className="shop-slide"
+                    >
+
+                        <SwiperSlide>
+                            <div className="w-[276px]">
+                                <div className="rounded-[20px] bg-[#e0e7ff] cursor-pointer relative overflow-hidden">
+                                    <span className="bg-black opacity-0 hover:opacity-[0.1] absolute left-0 top-0 w-full h-full transition-opacity"></span>
+                                    <img src="https://ciseco-reactjs.vercel.app/static/media/department1.8c732cf70fb1db8a427c.png" alt="clothes" />
+                                </div>
+                                <div className="text-center p-5">
+                                    <h2 className="text-[18px] font-semibold mb-1">Travel Kits</h2>
+                                    <p className="text-[14px] text-[#6B7280]">20+ categories</p>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+
+                        <SwiperSlide>
+                            <div className="w-[276px]">
+                                <div className="rounded-[20px] bg-[#f1f5f9] cursor-pointer relative overflow-hidden">
+                                    <span className="bg-black opacity-0 hover:opacity-[0.1] absolute left-0 top-0 w-full h-full transition-opacity"></span>
+                                    <img src="https://ciseco-reactjs.vercel.app/static/media/department2.0f6d094cc355eea274f9.png" alt="clothes" />
+                                </div>
+                                <div className="text-center p-5">
+                                    <h2 className="text-[18px] font-semibold mb-1">Travel Kits</h2>
+                                    <p className="text-[14px] text-[#6B7280]">20+ categories</p>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+
+                        <SwiperSlide>
+                            <div className="w-[276px]">
+                                <div className="rounded-[20px] bg-[#e0f2fe] cursor-pointer relative overflow-hidden">
+                                    <span className="bg-black opacity-0 hover:opacity-[0.1] absolute left-0 top-0 w-full h-full transition-opacity"></span>
+                                    <img src="https://ciseco-reactjs.vercel.app/static/media/department3.03411a1852db06674802.png" alt="clothes" />
+                                </div>
+                                <div className="text-center p-5">
+                                    <h2 className="text-[18px] font-semibold mb-1">Travel Kits</h2>
+                                    <p className="text-[14px] text-[#6B7280]">20+ categories</p>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+
+                        <SwiperSlide>
+                            <div className="w-[276px]">
+                                <div className="rounded-[20px] bg-[#ffedd5] cursor-pointer relative overflow-hidden">
+                                    <span className="bg-black opacity-0 hover:opacity-[0.1] absolute left-0 top-0 w-full h-full transition-opacity"></span>
+                                    <img src="https://ciseco-reactjs.vercel.app/static/media/department4.f4edc8706f19e8dbbd78.png" alt="clothes" />
+                                </div>
+                                <div className="text-center p-5">
+                                    <h2 className="text-[18px] font-semibold mb-1">Travel Kits</h2>
+                                    <p className="text-[14px] text-[#6B7280]">20+ categories</p>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+
+                        <SwiperSlide>
+                            <div className="w-[276px]">
+                                <div className="rounded-[20px] bg-[#e0e7ff] cursor-pointer relative overflow-hidden">
+                                    <span className="bg-black opacity-0 hover:opacity-[0.1] absolute left-0 top-0 w-full h-full transition-opacity"></span>
+                                    <img src="https://ciseco-reactjs.vercel.app/static/media/6.ed4616e785835c927ad1.png" alt="clothes" />
+                                </div>
+                                <div className="text-center p-5">
+                                    <h2 className="text-[18px] font-semibold mb-1">Umbrella</h2>
+                                    <p className="text-[14px] text-[#6B7280]">11+ categories</p>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                        <div className="button-next-slide w-[30px] h-[30px] absolute top-0 right-0 cursor-pointer hover:border duration-200 rounded-full flex items-center justify-center">
+                            <i class='bx bx-right-arrow-alt text-[20px]' ></i>
+                        </div>
+                        <div className="button-prev-slide w-[30px] h-[30px] cursor-pointer absolute top-0 right-[40px] hover:border duration-200 rounded-full flex items-center justify-center">
+                            <i class='bx bx-left-arrow-alt text-[20px]'></i>
+                        </div>
+                    </Swiper>
+{/* 
+                    <div className="flex justify-between">
+
+                    </div> */}
+                </div>
+            </div>
         </section>
     );
 };

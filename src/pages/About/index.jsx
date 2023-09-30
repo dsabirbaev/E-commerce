89c0fb9cd335a7eab9bde1c0c62265b380a94a40
Davranbek <davran9559@gmail.com>
2023-09-30 22:49:37 +0500

    add info

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -6,7 +6,9 @@ const index = () => {
         <section className="pt-[100px]">
             <div className="container">
                 <div className="h-screen">
-                    <h1>About</h1>
+                    <p className="text-lg dark:text-white">
+                        <span className="font-bold">Eclo</span>, the cutting-edge e-commerce platform, revolutionizes online shopping with its user-friendly interface and robust features. From seamless product browsing to secure transactions and personalized recommendations, Eclo ensures a top-notch shopping experience. With its advanced analytics, sellers can optimize their offerings, making Eclo a game-changer in the e-commerce industry.
+                    </p>
                 </div>
             </div>
         </section>
--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -9,7 +9,12 @@ const index = () => {
         <section className="pt-[100px]">
             <div className="container">
                 <div className="h-screen">
-                    <h1>Contact</h1>
+                    <h1 className="font-semibold text-4xl dark:text-white">Sabirbayev Davranbek</h1>
+                    <p className="text-lg mb-2 dark:text-white">Frontend developer</p>
+                    <hr />
+                    <p className="mt-2 mb-2 text-[18px] flex items-center gap-x-2 dark:text-white"><span><i class='bx bx-map dark:text-white'></i></span> Uzbekistan, Tashkent</p>
+                    <p className="mt-2 mb-2 text-[18px] flex items-center gap-x-2 dark:text-white"><span><i class='bx bx-envelope dark:text-white'></i></span> davran9559@gmail.com</p>
+                    <p className="mt-2 mb-2 text-[18px] flex items-center gap-x-2 dark:text-white"><span><i class='bx bx-phone dark:text-white'></i></span> +998 93 361 94 47</p>
                 </div>
             </div>
         </section>

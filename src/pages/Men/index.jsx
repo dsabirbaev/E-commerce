ceaa3c1f3c84ee3f2155adaca4115e4486094629
Davranbek <davran9559@gmail.com>
2023-09-30 19:11:29 +0500

    add sub categories

--- src/pages/Men/index.jsx
+++ src/pages/Men/index.jsx
@@ -8,10 +8,11 @@ import { Autoplay, Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/bundle';
 import useBrand from "../../services/brand/useBrand";
+import useSubCategory from "../../services/category/useCategory";
 const index = () => {
     const [brand, setBrand] = useState([]);
-
-    const getBrand = async() => {
+    const [category, setCategory] = useState([]);
+    const getBrand = async () => {
         try {
             const response = await useBrand.getBrand();
             setBrand(response.data);
@@ -20,8 +21,18 @@ const index = () => {
         }
     }
 
+    const getSubCategory = async () => {
+        try {
+            const response = await useSubCategory.getSubCategory();
+            setCategory(response.data);
+        } catch (err) {
+            console.log(err.message)
+        }
+    }
+
     useEffect(() => {
         getBrand();
+        getSubCategory();
     }, [])
     return (
 
@@ -32,41 +43,21 @@ const index = () => {
                     <aside className="w-[25%] rounded-md shadow p-5">
                         <div className="mb-10">
                             <h3 className="mb-4 font-semibold text-gray-900 dark:text-white">Categories</h3>
-                            <ul className="text-sm font-medium text-gray-900 bg-white  dark:bg-gray-700  dark:text-white">
-                                <li className="w-full hover:shadow-lg">
-                                    <div className="flex items-center pl-3">
-                                        <input id="backpack" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
-                                        <label htmlFor="backpack" className="w-full py-3 ml-2 text-md font-medium text-gray-900 dark:text-gray-300">Backpacks</label>
-                                    </div>
-                                </li>
+                            <ul className="text-sm font-medium text-gray-900 bg-white  dark:bg-transparent border dark:border-gray-400  dark:text-white rounded-md">
 
-                                <li className="w-full hover:shadow-lg">
-                                    <div className="flex items-center pl-3">
-                                        <input id="travel" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
-                                        <label htmlFor="travel" className="w-full py-3 ml-2 text-md font-medium text-gray-900 dark:text-gray-300">Travel Bags</label>
-                                    </div>
-                                </li>
+                                {
+                                    category.length ? category?.map((item) => (
+                                        <li key={item.id} className="w-full hover:shadow-lg dark:hover:shadow-xl">
+                                            <div className="flex items-center pl-3">
+                                                <input id={item?.id} type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                                                <label htmlFor={item?.id} className=" cursor-pointer w-full py-3 ml-2 text-md font-medium text-gray-900 dark:text-gray-300">{item?.name}</label>
+                                            </div>
+                                        </li>
+                                    )): <h1 className="text-center text-2xl">Category Not Found</h1>
+                               }
 
-                                <li className="w-full hover:shadow-lg">
-                                    <div className="flex items-center pl-3">
-                                        <input id="laptop" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
-                                        <label htmlFor="laptop" className="w-full py-3 ml-2 text-md font-medium text-gray-900 dark:text-gray-300">Laptop Sleeves</label>
-                                    </div>
-                                </li>
 
-                                <li className="w-full hover:shadow-lg">
-                                    <div className="flex items-center pl-3">
-                                        <input id="organization" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
-                                        <label htmlFor="organization" className="w-full py-3 ml-2 text-md font-medium text-gray-900 dark:text-gray-300">Organization</label>
-                                    </div>
-                                </li>
 
-                                <li className="w-full hover:shadow-lg">
-                                    <div className="flex items-center pl-3">
-                                        <input id="accessories" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
-                                        <label htmlFor="accessories" className="w-full py-3 ml-2 text-md font-medium text-gray-900 dark:text-gray-300">Accessories</label>
-                                    </div>
-                                </li>
                             </ul>
 
                         </div>
@@ -126,36 +117,36 @@ const index = () => {
                     <div className="wrapper  w-[75%]">
                         <div className="brands flex h-[330px] relative">
                             {
-                                brand.length ? 
-                                <Swiper
-                                spaceBetween={20}
-                                slidesPerView={3}
-                                slidesPerGroup={2}
-                                pagination={{ clickable: true }}
-                                modules={[ Pagination, Autoplay]}
-                                loop={true}
-                                autoplay={{
-                                    delay: 2500,
-                                    disableOnInteraction: false,
-                                }}
-                                centeredSlides={true}
-                             
-                                className="brand-slider"
-                                
-                            >
-                                {
-                                    brand?.map((item) => (
-                                        <SwiperSlide className="flex pt-[10px] justify-center">
-                                            <Brand key={item.id} case={item} path={{ imgPath }} />
-                                        </SwiperSlide>
+                                brand.length ?
+                                    <Swiper
+                                        spaceBetween={20}
+                                        slidesPerView={3}
+                                        slidesPerGroup={2}
+                                        pagination={{ clickable: true }}
+                                        modules={[Pagination, Autoplay]}
+                                        loop={true}
+                                        autoplay={{
+                                            delay: 2500,
+                                            disableOnInteraction: false,
+                                        }}
+                                        centeredSlides={true}
 
-                                    ))
-                                }
+                                        className="brand-slider"
 
-                              
-                                </Swiper>:  <h1 className="text-center text-2xl">Data Not Found</h1>
+                                    >
+                                        {
+                                            brand?.map((item) => (
+                                                <SwiperSlide className="flex pt-[10px] justify-center">
+                                                    <Brand key={item.id} case={item} path={{ imgPath }} />
+                                                </SwiperSlide>
+
+                                            ))
+                                        }
+
+
+                                    </Swiper> : <h1 className="text-center text-2xl">Data Not Found</h1>
                             }
-                           
+
 
                         </div>
                     </div>
--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,91 +1,73 @@
 
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 import useUser from "../../services/user/useUser";
-// const baseURL = "http://eclo.uz:8080/api";
+import { ToastContainer, toast } from 'react-toastify';
 import "./style.scss";
 const index = () => {
 
     const getToken = localStorage.getItem("token");
-    
-    const [data, setData] = useState({
-        firstName: "",
-        lastName: "",
-        passportSerialNumber: "",
-        birthDate: "",
-        region: "",
-        district: "",
-        address: "",
-        imagePath: ""
-    });
-   
-    const [avatar, setAvatar] = useState('');
-   
-    // const [name, setName] = useState("");
-    // const [surname, setSurname] = useState("");
-    // const [passportSerialNumber, setPassportSerialNumber] = useState("");
-    // const [birthData, setBirthData] = useState("");
-    // const [region, setRegion] = useState("");
-    // const [district, setDistrict] = useState("");
-    // const [address, setAddress] = useState("");
+    const [data, setData] = useState([]);
 
-    async function getProfile() {
+   
+    const [imagePath, setImagePath] = useState('');
+   
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [passportSerialNumber, setPassportSerialNumber] = useState("");
+    const [birthDate, setBirthData] = useState("");
+    const [region, setRegion] = useState("");
+    const [district, setDistrict] = useState("");
+    const [address, setAddress] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
+
+    const getProfile = async() => {
         try{
             const response = await useUser.profile();
             setData(response.data);
-            console.log(response.data);  
+           
         }catch(error){
             console.log(error.message);
         }
     }
 
+    const updateProfile = () => {
+        const newAccount = {
+            firstName,
+            lastName,
+            passportSerialNumber,
+            birthDate,
+            region,
+            district,
+            address,
+            phoneNumber,
+            imagePath
+         }
+        
+        console.log(newAccount)
 
-    console.log("dd " + avatar);
-
-    const updateAccount = {
-        firstName: data.firstName,
-        lastName: data.lastName,
-        passportSerialNumber: data.passportSerialNumber,
-        birthDate: data.birthDate,
-        region: data.region,
-        district: data.district,
-        address: data.address,
-        imagePath: data.imagePath
+        useUser.profileUpdate(newAccount).then((res) => {
+            console.log(res)
+            toast.success("Update successful!", { autoClose: 1000 });
+        }).catch((err) => {
+            console.log(err)
+            toast.error("Update error!", { autoClose: 1000 });
+        })
     }
-
  
-    async function updateProfile() {
-        try {
-            const response = await axios.put(`${baseURL}/user/profile/userId`, updateAccount, {
-                headers: {
-                    'Authorization': `Bearer ${getToken}`
-                }
-            });
-            console.log("Profile updated successfully:", response.data);
-            setData(response.data); 
-        } catch (error) {
-            console.log(error.message);
-        }
-    }
-
    
-   
-   
-
     function submitHandle(e){
         e.preventDefault();
         updateProfile()
     }
 
-   
-    document.title = `Eclo | ${data.firstName}`;
-
-    useEffect(() => {
+    useEffect(() => {    
         getProfile();
     }, [])
     
-    
+    document.title = `Eclo | ${data.firstName}`;
     return (
         <section className="py-[100px]">
+            <ToastContainer />
             <div className="container">
                 <div className="flex items-center justify-center">
                     <div className="w-[800px] rounded-lg p-5">
@@ -105,42 +87,49 @@ const index = () => {
 
                         
                         <form onSubmit={submitHandle} className="flex gap-x-16">
-                            <img className="w-[128px] h-[128px] rounded-full bg-slate-400" src={avatar} alt="avatar" />
+                          
+                            <input type="file" accept='jpg/png' onChange={(e) => setImagePath(e.target.files[0])} />
                             <div className="w-[80%]">
                                 <div className="relative z-0 w-full mb-6 group">
-                                    <input value={data.firstName} onChange={(e) => setData(prevData => ({ ...prevData, firstName: e.target.value }))} type="text" name="floating_first_name" id="floating_first_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+                                    <input defaultValue={data?.firstName}  onChange={(e) => setFirstName(e.target.value)} type="text" name="floating_first_name" id="floating_first_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
                                     <label htmlFor="floating_first_name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">First name</label>
+                                    
                                 </div>
                                 <div className="relative z-0 w-full mb-6 group">
-                                    <input value={data.lastName} onChange={(e) => setData(prevData => ({ ...prevData, lastName: e.target.value }))} type="text" name="floating_last_name" id="floating_last_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+                                    <input defaultValue={data?.lastName} onChange={(e) => setLastName(e.target.value)} type="text" name="floating_last_name" id="floating_last_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
                                     <label htmlFor="floating_last_name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Last name</label>
                                 </div>
 
                                 <div className="relative z-0 w-full mb-8 group">
-                                    <input value={data.passportSerialNumber} onChange={(e) => setData(prevData => ({ ...prevData, passportSerialNumber: e.target.value }))} type="text" name="floating_passport_serial_number" id="floating_passport_serial_number" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+                                    <input defaultValue={data?.passportSerialNumber} onChange={(e) => setPassportSerialNumber(e.target.value)} type="text" name="floating_passport_serial_number" id="floating_passport_serial_number" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
                                     <label htmlFor="floating_passport_serial_number" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Passport Serial Number</label>
                                 </div>
 
                                 <div className="grid md:grid-cols-2 md:gap-6">
                                     <div className="relative z-0 w-full mb-6 group">
-                                        <input value={data.birthDate} onChange={(e) => setData(prevData => ({ ...prevData, birthDate: e.target.value }))} type="date" name="floating_birth" id="floating_birth" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+                                        <input defaultValue={data?.birthDate} onChange={(e) => setBirthData(e.target.value)} type="date" name="floating_birth" id="floating_birth" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
                                         <label htmlFor="floating_birth" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Birth Date</label>
                                     </div>
                                     <div className="relative z-0 w-full mb-6 group">
-                                        <input value={data.region} onChange={(e) => setData(prevData => ({ ...prevData, region: e.target.value }))} type="text" name="floating_region" id="floating_region" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+                                        <input defaultValue={data?.region} onChange={(e) => setRegion(e.target.value)} type="text" name="floating_region" id="floating_region" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
                                         <label htmlFor="floating_region" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Region</label>
                                     </div>
                                 </div>
 
                                 <div className="grid md:grid-cols-2 md:gap-6">
                                     <div className="relative z-0 w-full mb-6 group">
-                                        <input value={data.district} onChange={(e) => setData(prevData => ({ ...prevData, district: e.target.value }))} type="text" name="floating_district" id="floating_district" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+                                        <input defaultValue={data?.district} onChange={(e) => setDistrict(e.target.value)} type="text" name="floating_district" id="floating_district" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
                                         <label htmlFor="floating_district" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">District</label>
                                     </div>
                                     <div className="relative z-0 w-full mb-6 group">
-                                        <input value={data.address} onChange={(e) => setData(prevData => ({ ...prevData, address: e.target.value }))} type="text" name="floating_address" id="floating_address" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+                                        <input defaultValue={data?.address} onChange={(e) => setAddress(e.target.value)} type="text" name="floating_address" id="floating_address" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
                                         <label htmlFor="floating_address" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Address</label>
                                     </div>
+                                </div>
+
+                                <div className="relative z-0 w-full mb-8 group">
+                                    <input defaultValue={data?.phoneNumber}  onChange={(e) => setPhoneNumber(e.target.value)} type="text" name="floating_floating_telephone_number_number" id="floating_telephone_number" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+                                    <label htmlFor="floating_telephone_number" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Telephone Number</label>
                                 </div>
 
                                 <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Update account</button>

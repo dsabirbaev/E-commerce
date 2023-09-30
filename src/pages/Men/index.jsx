b57031cc23c18ac16f8ecb8ccd6f0aff6fb29fc9
Davranbek <davran9559@gmail.com>
2023-09-30 19:55:42 +0500

    add size

--- src/pages/Men/index.jsx
+++ src/pages/Men/index.jsx
@@ -9,9 +9,11 @@ import 'swiper/css';
 import 'swiper/css/bundle';
 import useBrand from "../../services/brand/useBrand";
 import useSubCategory from "../../services/category/useCategory";
+import useSize from "../../services/size/useSize";
 const index = () => {
     const [brand, setBrand] = useState([]);
     const [category, setCategory] = useState([]);
+    const [size, setSize] = useState([]);
     const getBrand = async () => {
         try {
             const response = await useBrand.getBrand();
@@ -30,10 +32,29 @@ const index = () => {
         }
     }
 
+    const getSize = async () => {
+        try {
+            const response = await useSize.getSize();
+            setSize(response.data);
+
+        } catch (err) {
+            console.log(err.message)
+        }
+    }
+
     useEffect(() => {
         getBrand();
         getSubCategory();
+        getSize();
     }, [])
+
+    const uniqueSize = [];
+    if(size.length){
+        size.map((item) => uniqueSize.push(item.size))
+    }
+    const sortedArr = [...new Set(uniqueSize)];
+    
+    
     return (
 
         <section className="py-[100px]" >
@@ -64,48 +85,19 @@ const index = () => {
 
                         <div>
                             <h3 className="mb-4 font-semibold text-gray-900 dark:text-white">Sizes</h3>
-                            <ul className="text-sm font-medium text-gray-900 bg-white  dark:bg-gray-700  dark:text-white">
-                                <li className="w-full hover:shadow-lg">
-                                    <div className="flex items-center pl-3">
-                                        <input id="xs" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
-                                        <label htmlFor="xs" className="w-full py-3 ml-2 text-md font-medium text-gray-900 dark:text-gray-300">XS</label>
-                                    </div>
-                                </li>
-
-                                <li className="w-full hover:shadow-lg">
-                                    <div className="flex items-center pl-3">
-                                        <input id="s" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
-                                        <label htmlFor="s" className="w-full py-3 ml-2 text-md font-medium text-gray-900 dark:text-gray-300">S</label>
-                                    </div>
-                                </li>
-
-                                <li className="w-full hover:shadow-lg">
-                                    <div className="flex items-center pl-3">
-                                        <input id="m" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
-                                        <label htmlFor="m" className="w-full py-3 ml-2 text-md font-medium text-gray-900 dark:text-gray-300">M</label>
-                                    </div>
-                                </li>
-
-                                <li className="w-full hover:shadow-lg">
-                                    <div className="flex items-center pl-3">
-                                        <input id="l" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
-                                        <label htmlFor="l" className="w-full py-3 ml-2 text-md font-medium text-gray-900 dark:text-gray-300">L</label>
-                                    </div>
-                                </li>
-
-                                <li className="w-full hover:shadow-lg">
-                                    <div className="flex items-center pl-3">
-                                        <input id="xl" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
-                                        <label htmlFor="xl" className="w-full py-3 ml-2 text-md font-medium text-gray-900 dark:text-gray-300">XL</label>
-                                    </div>
-                                </li>
-
-                                <li className="w-full hover:shadow-lg">
-                                    <div className="flex items-center pl-3">
-                                        <input id="2xl" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
-                                        <label htmlFor="2xl" className="w-full py-3 ml-2 text-md font-medium text-gray-900 dark:text-gray-300">2XL</label>
-                                    </div>
-                                </li>
+                            <ul className="text-sm font-medium text-gray-900 bg-white  dark:bg-transparent border dark:border-gray-400  dark:text-white rounded-md">
+                               
+                                {
+                                    sortedArr.length ? sortedArr?.map((item, index) => (
+                                        <li key={Date.now()+index} className="w-full hover:shadow-lg dark:hover:shadow-xl">
+                                            <div className="flex items-center pl-3">
+                                                <input id={Date.now()+index} type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+                                                <label htmlFor={Date.now()+index} className=" cursor-pointer w-full py-3 ml-2 text-md font-medium text-gray-900 dark:text-gray-300"> {item}</label>
+                                            </div>
+                                        </li>
+                                    )): <h1 className="text-center text-2xl">Size Not Found</h1>
+                               }
+                               
                             </ul>
 
                         </div>
@@ -136,8 +128,8 @@ const index = () => {
                                     >
                                         {
                                             brand?.map((item) => (
-                                                <SwiperSlide className="flex pt-[10px] justify-center">
-                                                    <Brand key={item.id} case={item} path={{ imgPath }} />
+                                                <SwiperSlide key={item.id} className="flex pt-[10px] justify-center">
+                                                    <Brand  case={item} path={{ imgPath }} />
                                                 </SwiperSlide>
 
                                             ))

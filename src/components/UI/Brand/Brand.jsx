83d8c3058deb4316fbd6e7581400e6354821ed7d
Davranbek <davran9559@gmail.com>
2023-10-22 23:47:42 +0500

    fixed api profile and brands

--- src/components/UI/Brand/Brand.jsx
+++ src/components/UI/Brand/Brand.jsx
@@ -1,14 +1,14 @@
 
 
-const imgPath = "http://68.183.181.125:89";
+const imgPath = "http://165.22.110.111:8080";
 import "./style.scss";
 
 const Brand = ({case:{name, brandIconPath}}) => {
-    console.log(brandIconPath)
+    
     return (
         <div className="brand">
             <div className="hover:shadow-lg  cursor-pointer w-[250px] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                <img className="rounded-t-lg  h-[200px] w-full object-center object-contain" src={`${imgPath}/${brandIconPath.replace('\\', '/')}`} alt="" />
+                <img className="rounded-t-lg  h-[200px] w-full object-center object-contain" src={`${imgPath}/${brandIconPath.replace('\\', '/')}`} alt="image" />
                 <div className="p-5">
                     <h5 className="text-center mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{name}</h5>
                 </div>
--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -2,15 +2,16 @@
 import { useEffect, useState } from "react";
 import useUser from "../../services/user/useUser";
 import { ToastContainer, toast } from 'react-toastify';
+const imgPath = "http://165.22.110.111:8080";
 import "./style.scss";
 const index = () => {
 
     const getToken = localStorage.getItem("token");
     const [data, setData] = useState([]);
 
-   
+
     const [imagePath, setImagePath] = useState('');
-   
+
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
     const [passportSerialNumber, setPassportSerialNumber] = useState("");
@@ -20,13 +21,14 @@ const index = () => {
     const [address, setAddress] = useState("");
     const [phoneNumber, setPhoneNumber] = useState("");
 
-    const getProfile = async() => {
-        try{
+    const getProfile = async () => {
+        try {
             const response = await useUser.profile();
             setData(response.data);
-           
-        }catch(error){
+
+        } catch (error) {
             console.log(error.message);
+            console.log(error)
         }
     }
 
@@ -41,8 +43,8 @@ const index = () => {
             address,
             phoneNumber,
             imagePath
-         }
-        
+        }
+
         console.log(newAccount)
 
         useUser.profileUpdate(newAccount).then((res) => {
@@ -53,17 +55,18 @@ const index = () => {
             toast.error("Update error!", { autoClose: 1000 });
         })
     }
- 
-   
-    function submitHandle(e){
+
+
+    function submitHandle(e) {
         e.preventDefault();
         updateProfile()
     }
 
-    useEffect(() => {    
+    useEffect(() => {
         getProfile();
     }, [])
-    
+
+
     document.title = `Eclo | ${data.firstName}`;
     return (
         <section className="py-[100px]">
@@ -85,15 +88,17 @@ const index = () => {
 
                         <h2 className="font-semibold text-[30px] text-[#0F172A] mb-8">Account infomation</h2>
 
-                        
+
                         <form onSubmit={submitHandle} className="flex gap-x-16">
-                          
-                            <input type="file" accept='jpg/png' onChange={(e) => setImagePath(e.target.files[0])} />
+                            <div className="flex flex-col gap-y-5">
+                                <img className="w-40 h-40 rounded" src={`${imgPath}/${data.imagePath}`} alt="Large avatar" />
+                                <input type="file" accept='jpg/png' onChange={(e) => setImagePath(e.target.files[0])} />
+                            </div>
                             <div className="w-[80%]">
                                 <div className="relative z-0 w-full mb-6 group">
-                                    <input defaultValue={data?.firstName}  onChange={(e) => setFirstName(e.target.value)} type="text" name="floating_first_name" id="floating_first_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+                                    <input defaultValue={data?.firstName} onChange={(e) => setFirstName(e.target.value)} type="text" name="floating_first_name" id="floating_first_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
                                     <label htmlFor="floating_first_name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">First name</label>
-                                    
+
                                 </div>
                                 <div className="relative z-0 w-full mb-6 group">
                                     <input defaultValue={data?.lastName} onChange={(e) => setLastName(e.target.value)} type="text" name="floating_last_name" id="floating_last_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
@@ -128,7 +133,7 @@ const index = () => {
                                 </div>
 
                                 <div className="relative z-0 w-full mb-8 group">
-                                    <input defaultValue={data?.phoneNumber}  onChange={(e) => setPhoneNumber(e.target.value)} type="text" name="floating_floating_telephone_number_number" id="floating_telephone_number" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+                                    <input defaultValue={data?.phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} type="text" name="floating_floating_telephone_number_number" id="floating_telephone_number" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
                                     <label htmlFor="floating_telephone_number" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Telephone Number</label>
                                 </div>
 
